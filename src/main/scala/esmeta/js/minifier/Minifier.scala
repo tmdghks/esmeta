21dd270e11e5c8633f84d7c92b832fa7c9b2c704
SeungHwan Kim <tmdghks12@korea.ac.kr>
2025-02-03 19:34:58 +0900

    modify minifier version

--- src/main/scala/esmeta/js/minifier/Minifier.scala
+++ src/main/scala/esmeta/js/minifier/Minifier.scala
@@ -16,14 +16,14 @@ import sttp.model.StatusCode
 
 object Minifier {
   val minifyCmd = Map(
-    "swc" -> "minify-runner -v swc@1.4.6",
-    "swcES2015" -> "minify-runner -v swc@1.4.6 --notcompress -t es2015", // todo? refactor to use general minify command with options
-    "terser" -> "minify-runner -v terser@5.29.1",
-    "babel" -> "minify-runner -v babel@7.24.1",
-    "checkDiffSwc" -> "minify-runner -v swc@1.4.6 -d",
-    "checkDiffSwcES2015" -> "minify-runner -v swc@1.4.6 --notcompress -t es2015 -d",
-    "checkDiffTerser" -> "minify-runner -v terser@5.29.1 -d",
-    "checkDiffBabel" -> "minify-runner -v babel@7.24.1 -d",
+    "swc" -> "minify-runner -v swc@1.3.10",
+    "swcES2015" -> "minify-runner -v swc@1.3.10 --notcompress -t es2015", // todo? refactor to use general minify command with options
+    "terser" -> "minify-runner -v terser@5.15.1",
+    "babel" -> "minify-runner -v babel@7.19.1",
+    "checkDiffSwc" -> "minify-runner -v swc@1.3.10 -d",
+    "checkDiffSwcES2015" -> "minify-runner -v swc@1.3.10 --notcompress -t es2015 -d",
+    "checkDiffTerser" -> "minify-runner -v terser@5.15.1 -d",
+    "checkDiffBabel" -> "minify-runner -v babel@7.19.1 -d",
   )
 
   lazy val useSwc: Boolean = minifySwc(";").isSuccess
@@ -127,12 +127,12 @@ object MinifyServer {
   def queryDiff(code: String, cmd: Option[String]): String = {
     val version = cmd match
       case Some("swc") | Some("Swc") | Some("swcES2015") | Some("SwcES2015") =>
-        "swc@1.4.6"
-      case Some("terser") | Some("Terser") => "terser@5.29.1"
-      case Some("babel") | Some("Babel")   => "babel@7.24.1"
+        "swc@1.3.10"
+      case Some("terser") | Some("Terser") => "terser@5.15.1"
+      case Some("babel") | Some("Babel")   => "babel@7.19.1"
       case None =>
         Minifier.warnUnspecified()
-        "swc@1.4.6"
+        "swc@1.3.10"
       case _ => throw new Exception("Invalid minifier specified.")
 
     val encodedCode = URLEncoder.encode(code, "UTF-8")
